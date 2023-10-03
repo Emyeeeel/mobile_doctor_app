35be2f5b6b1d9d628e46d269f84e8308d28c604e
Emyeeeel <maryamielcatado@gmail.com>
2023-10-03 22:39:50 +0800

    finished home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_app/widgets/listview_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -118,7 +119,7 @@ class _HomeScreenState extends State<Homescreen> {
                                 decoration: const BoxDecoration(
                                     image: DecorationImage(
                                         image: AssetImage(
-                                            'assets/icons/Search.png'))),
+                                            'assets/icons/007-stethoscope.png'))),
                               ),
                             ),
                             Padding(
@@ -132,9 +133,337 @@ class _HomeScreenState extends State<Homescreen> {
                               ),
                             )
                           ],
+                        )),
+                    const SizedBox(
+                      width: 27,
+                    ),
+                    SizedBox(
+                        width: 56,
+                        height: 81,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 56,
+                              height: 56,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFFA584FF),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Container(
+                                width: 24,
+                                height: 24,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/icons/004-teeth.png'))),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                              child: Text(
+                                'Dental',
+                                style: GoogleFonts.lato(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                    color: const Color(0xFF25282B)),
+                              ),
+                            )
+                          ],
+                        )),
+                    const SizedBox(
+                      width: 34,
+                    ),
+                    SizedBox(
+                        width: 56,
+                        height: 81,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 56,
+                              height: 56,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFFFF7854),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Container(
+                                width: 24,
+                                height: 24,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/icons/017-heart.png'))),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                              child: Text(
+                                'Heart',
+                                style: GoogleFonts.lato(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                    color: const Color(0xFF25282B)),
+                              ),
+                            )
+                          ],
+                        )),
+                    const SizedBox(
+                      width: 34,
+                    ),
+                    SizedBox(
+                        width: 56,
+                        height: 81,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 56,
+                              height: 56,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFFFFB547),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Container(
+                                width: 24,
+                                height: 24,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/icons/024-clinic.png'))),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                              child: Text(
+                                'Hospitals',
+                                style: GoogleFonts.lato(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                    color: const Color(0xFF25282B)),
+                              ),
+                            )
+                          ],
                         ))
                   ],
-                )
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                        width: 69,
+                        height: 81,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 56,
+                              height: 56,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF00CC6A),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Container(
+                                width: 24,
+                                height: 24,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/icons/007-stethoscope.png'))),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                              child: Text(
+                                'Medicines',
+                                style: GoogleFonts.lato(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                    color: const Color(0xFF25282B)),
+                              ),
+                            )
+                          ],
+                        )),
+                    const SizedBox(
+                      width: 27,
+                    ),
+                    SizedBox(
+                        width: 56,
+                        height: 81,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 56,
+                              height: 56,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF05D1ED),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Container(
+                                width: 24,
+                                height: 24,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/icons/004-teeth.png'))),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                              child: Text(
+                                'Physician',
+                                style: GoogleFonts.lato(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                    color: const Color(0xFF25282B)),
+                              ),
+                            )
+                          ],
+                        )),
+                    const SizedBox(
+                      width: 34,
+                    ),
+                    SizedBox(
+                        width: 56,
+                        height: 81,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 56,
+                              height: 56,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFFFF71C6),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Container(
+                                width: 24,
+                                height: 24,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/icons/017-heart.png'))),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                              child: Text(
+                                'Skin',
+                                style: GoogleFonts.lato(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                    color: const Color(0xFF25282B)),
+                              ),
+                            )
+                          ],
+                        )),
+                    const SizedBox(
+                      width: 34,
+                    ),
+                    SizedBox(
+                        width: 56,
+                        height: 81,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 56,
+                              height: 56,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFFFF7070),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Container(
+                                width: 24,
+                                height: 24,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/icons/024-clinic.png'))),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                              child: Text(
+                                'Surgeon',
+                                style: GoogleFonts.lato(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                    color: const Color(0xFF25282B)),
+                              ),
+                            )
+                          ],
+                        ))
+                  ],
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Top Doctors',
+                      style: GoogleFonts.lato(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: const Color(0xFF25282B)),
+                    ),
+                    const SizedBox(
+                      width: 189,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      child: Text(
+                        'View all',
+                        style: GoogleFonts.lato(
+                            fontSize: 11,
+                            fontWeight: FontWeight.bold,
+                            color: const Color(0xFF4485FD)),
+                      ),
+                    )
+                  ],
+                ),
+                ListView.builder(
+                    itemCount: 5,
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return const VerticalList(
+                          image: 'assets/images/Group 22.png',
+                          name: 'Dr. Gilang Segara Bening',
+                          specialization: 'Heart',
+                          location: 'Persahabatan Hospital',
+                          reviews: '(1221)',
+                          rating: 5.0,
+                        );
+                      } else if (index == 1) {
+                        return const VerticalList(
+                          image: 'assets/images/Group 23.png',
+                          name: 'Dr. Shabil Chan',
+                          specialization: 'Dental',
+                          location: 'Columbia Asia Hospital',
+                          reviews: '(964)',
+                          rating: 5.0,
+                        );
+                      } else if (index == 2) {
+                        return const VerticalList1(
+                          image: 'assets/images/Group 24.png',
+                          name: 'Dr. Mustakim',
+                          specialization: 'Eye',
+                          location: 'Salemba Carolus Hospital',
+                          reviews: '(762)',
+                          rating: 5.0,
+                        );
+                      } else if (index == 3) {
+                        return const VerticalList(
+                          image: 'assets/images/Group 25.png',
+                          name: 'Dr. Suprihatini',
+                          specialization: 'Heart',
+                          location: 'Salemba Carolus Hospital',
+                          reviews: '(762)',
+                          rating: 5.0,
+                        );
+                      } else {
+                        return const VerticalList(
+                          image: 'assets/images/Group 22.png',
+                          name: 'Dr. Gilang Segara Bening',
+                          specialization: 'Heart',
+                          location: 'Persahabatan Hospital',
+                          reviews: '(1221)',
+                          rating: 5.0,
+                        );
+                      }
+                    })
               ],
             ),
           ],
---
+++ lib/widgets/listview_widgets.dart
@@ -0,0 +1,297 @@
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class VerticalList extends StatelessWidget {
+  final String image, name, specialization, location, reviews;
+  final double rating;
+
+  const VerticalList(
+      {super.key,
+      required this.image,
+      required this.name,
+      required this.specialization,
+      required this.location,
+      required this.reviews,
+      required this.rating});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        bottom: 16,
+        top: 24,
+      ),
+      child: SizedBox(
+        height: 80,
+        width: 327,
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  height: 80,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(image),
+                  ),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        name,
+                        style: GoogleFonts.lato(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: const Color(0xFF404345)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                        child: Row(
+                          children: [
+                            Text(
+                              specialization,
+                              style: GoogleFonts.lato(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: const Color(0xFFAAAAAA)),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Container(
+                              width: 3,
+                              height: 3,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color(0xFFAAAAAA),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Text(
+                              location,
+                              style: GoogleFonts.lato(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: const Color(0xFFAAAAAA)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Row(
+                        children: [
+                          RatingBar.builder(
+                            initialRating: rating,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            itemSize: 18,
+                            itemPadding:
+                                const EdgeInsets.symmetric(horizontal: 1),
+                            itemBuilder: (context, _) => const Icon(
+                              Icons.star,
+                              color: Color(0xFFF4C465),
+                            ),
+                            onRatingUpdate: (rating) {
+                              print(rating);
+                            },
+                          ),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          Text(
+                            reviews,
+                            style: GoogleFonts.lato(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w400,
+                                color: const Color(0xFFAAAAAA)),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                    width: 56,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      color: const Color(0xFFCCF5E1),
+                    ),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Open',
+                        style: GoogleFonts.lato(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: const Color(0xFF00CC6A)),
+                      ),
+                    )))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class VerticalList1 extends StatelessWidget {
+  final String image, name, specialization, location, reviews;
+  final double rating;
+
+  const VerticalList1(
+      {super.key,
+      required this.image,
+      required this.name,
+      required this.specialization,
+      required this.location,
+      required this.reviews,
+      required this.rating});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        bottom: 16,
+        top: 24,
+      ),
+      child: SizedBox(
+        height: 80,
+        width: 327,
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  height: 80,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(image),
+                  ),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        name,
+                        style: GoogleFonts.lato(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: const Color(0xFF404345)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                        child: Row(
+                          children: [
+                            Text(
+                              specialization,
+                              style: GoogleFonts.lato(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: const Color(0xFFAAAAAA)),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Container(
+                              width: 3,
+                              height: 3,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color(0xFFAAAAAA),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Text(
+                              location,
+                              style: GoogleFonts.lato(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: const Color(0xFFAAAAAA)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Row(
+                        children: [
+                          RatingBar.builder(
+                            initialRating: rating,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            itemSize: 18,
+                            itemPadding:
+                                const EdgeInsets.symmetric(horizontal: 1),
+                            itemBuilder: (context, _) => const Icon(
+                              Icons.star,
+                              color: Color(0xFFF4C465),
+                            ),
+                            onRatingUpdate: (rating) {
+                              print(rating);
+                            },
+                          ),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          Text(
+                            reviews,
+                            style: GoogleFonts.lato(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w400,
+                                color: const Color(0xFFAAAAAA)),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                    width: 56,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      color: const Color(0xFFF7E4D9),
+                    ),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Close',
+                        style: GoogleFonts.lato(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: const Color(0xFFCC4900)),
+                      ),
+                    )))
+          ],
+        ),
+      ),
+    );
+  }
+}
